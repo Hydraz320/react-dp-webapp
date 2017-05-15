2200a8c8cf6527c8031d89d941d9cab2178f2fbb
Gezhen <gezhen_auto@163.com>
2017-05-15 09:50:14 +0800

    koa server

---
+++ mock/server.js
@@ -0,0 +1,31 @@
+/**
+ * Created by hydra320 on 2017/5/14.
+ */
+const Koa = require('koa')
+const Router = require('koa-router')
+
+const app = new Koa()
+const router = new Router()
+
+// home首页 超值特惠
+let homeAdData = require('./home/ad')
+router.get('/api/homead', async (ctx) => {
+  ctx.body = homeAdData
+})
+
+// home首页 猜你喜欢
+let homeListData = require('./home/list')
+router.get('/api/homelist/:city/:page', async (ctx) => {
+  const params = this.params
+  const paramsCity = params.city
+  const paramsPage = params.page
+
+  console.log('当前城市：' + paramsCity)
+  console.log('当前页数：' + paramsPage)
+
+  ctx.body = homeListData
+});
+
+app.use(router.route(), router.allowedMethods())
+app.listen(3000)
+console.log('server listening -p 3000...')
